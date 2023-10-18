a5f4b8f7fb5685ae8f606a4c9af9cda54705ed46
Varun Verma <vermavarun@outlook.com>
2023-10-18 20:14:26 +0530

    hugging face added

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.embeddings import OpenAIEmbeddings
 from dotenv import load_dotenv
 
+from transformers import pipeline
+import requests
+
 load_dotenv()
 
 def test():
@@ -25,6 +28,7 @@ def data_ingestion():
     print("loader ready")
     index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
     print("Data Ingestion Done")
+
 def ask():
 
     question = input("Ask anything about the data\n")
@@ -47,7 +51,33 @@ def ask():
         print(doc.metadata['page'])
         print(doc.page_content)
 
+def hugging_face():
+    call_api = True # we can download the model or directly call the api from hugging face
+    # https://huggingface.co/tasks/image-to-text
+    print("HF")
+
+    if call_api:
+        print("Calling API")
+        hugging_face_api_key = os.environ['HUGGING_FACE_API']
+        API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-base" # https://huggingface.co/Salesforce/blip-image-captioning-base?inference_api=true
+        headers = {"Authorization": "Bearer " + hugging_face_api_key}
+        path_of_image = "C:\\Users\\varun.verma\\OneDrive - Shell\\Documents\\My Pictures\\me-maa.png"
+
+        with open(path_of_image, "rb") as f:
+            data = f.read()
+            response = requests.post(API_URL, headers=headers, data=data)
+            print(response.json()[0]['generated_text'])
+
+    else:
+        print("Using Downloaded Module")
+        url="https://avatars.githubusercontent.com/u/8264476?v=4"
+        image_to_text = pipeline("image-to-text",model="Salesforce/blip-image-captioning-base") # "https://huggingface.co/Salesforce/blip-image-captioning-base" #https://huggingface.co/Salesforce/blip-image-captioning-large?library=true
+        text = image_to_text(url)[0]["generated_text"]
+        print(text)
+
+
 if __name__ == "__main__":
-    #test()
-    #data_ingestion()
-    ask()
+    # test()
+    # data_ingestion()
+    # ask()
+    hugging_face()
