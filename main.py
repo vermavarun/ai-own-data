4fc799ef2e89d4373ff7cd419910c46134eed9b8
Varun Verma <vermavarun@outlook.com>
2023-10-18 15:42:55 +0530

    data ingestion and qa done :)

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import os
 import sys
 import openai
 from langchain.llms import OpenAI
-from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.chat_models import ChatOpenAI
 from langchain.indexes import VectorstoreIndexCreator
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.vectorstores import Chroma
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 
+from langchain.embeddings import OpenAIEmbeddings
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,10 +21,27 @@ def test():
 
 def data_ingestion():
     print("Starting Data Ingestion")
-    loader = DirectoryLoader("data_dir/")
+    loader = PyPDFDirectoryLoader("data_dir/") # check PyPDFDirectoryLoader #https://github.com/mgleavitt/my_directory_loader/blob/master/my_directory_loader.py
+    print("loader ready")
     index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
     print("Data Ingestion Done")
+def ask():
+
+    question = input("Ask anything about the data\n")
+
+    vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+    index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(model="gpt-3.5-turbo"),
+        retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+        )
+    chat_history = []
+    result  = chain({"question": question,"chat_history":chat_history})
+    print(result)
+    print(result['answer'])
 
 if __name__ == "__main__":
     #test()
-    data_ingestion()
+    #data_ingestion()
+    ask()
